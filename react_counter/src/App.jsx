c8b61e51c60f6aab9936e096b79edac380a7269c
ngsw <ngsw993@gmail.com>
2024-12-11 23:28:04 +0900

    カウント要素追加

--- react_counter/src/App.jsx
+++ react_counter/src/App.jsx
@@ -4,11 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const handleIncrease =() => {
+    setCount((prevCount) => prevCount + 1)
+  }
+  const handleDecrease =() => {
+    setCount((prevCount) => prevCount - 1)
+  }
+  const style = {
+    color: count >= 0 ? 'green' : 'red'
+  };
 
   return (
     <>
       <div>
-        <p>ハロー</p>
+        <h1 style={style}>現在のカウント:{count}</h1>
+        <button onClick={handleIncrease}>+</button>
+        <button onClick={handleDecrease}>-</button>
       </div>
     </>
   )
